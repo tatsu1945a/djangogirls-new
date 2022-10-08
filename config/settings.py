e38038e57c6e2af02e2e2c6c379c51fe73a89a6e
Tatsuhiro Akita <vmak8160@gmail.com>
2022-10-08 15:13:44 +0900

    Added views to create/edit blog post inside the site. end

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
