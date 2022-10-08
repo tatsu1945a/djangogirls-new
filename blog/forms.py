be70cc0e51fcd8a01ced353f5b9ae4c5a84b5afa
Tatsuhiro Akita <vmak8160@gmail.com>
2022-10-08 13:27:44 +0900

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,9 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
